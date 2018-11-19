bb3779361731bb38ef90fdab9de85f6490efda5e
carlos-santiago-2017 <c.santiago.cruz@gmail.com>
2018-11-19 00:27:31 -0600

    add example of how to work the delegate for safariviewcontrollers

---
+++ WorkingWithTheWeb/LoadingAwebSiteWithSafariViewControllerAssigningAdelegate/LoadingAwebSiteWithSafariViewController/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  LoadingAwebSiteWithSafariViewController
+//
+//  Created by Carlos Santiago Cruz on 11/18/18.
+//  Copyright © 2018 Carlos Santiago Cruz. All rights reserved.
+//
+
+import UIKit
+import SafariServices
+
+class ViewController: UIViewController, SFSafariViewControllerDelegate {
+    @IBOutlet weak var openWebButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    @IBAction func openWebButtonnTapped(_ sender: UIButton) {
+        let url = URL(string: "https://www.apple.com")
+        let controller = SFSafariViewController(url: url!)
+        controller.delegate = self
+        present(controller, animated: true, completion: nil)
+    }
+    func safariViewControllerDidFinish(_ controller: SFSafariViewController) {
+        openWebButton.isEnabled = false
+    }
+    // wow, tapped the button, but when you press done, the method disable the button openWebButton.
+    // remember, delegate patter comunicate viewcontroller, the SafariViewController send the message to disable the button of the first ViewCOntroller.
+
+}
+
