32f244d11ec88ddbfd208f5c66560a19a87d6c90
carlos-santiago-2017 <c.santiago.cruz@gmail.com>
2018-11-23 08:52:38 -0600

    commiting the result of use GrandCentralDispatch

---
+++ WorkingWithTheWeb/RequestNASAapodConcurrency/RequestNASAapod/PhotoInfoController.swift
@@ -0,0 +1,30 @@
+//
+//  PhotoInfoController.swift
+//  RequestNASAapod
+//
+//  Created by Carlos Santiago Cruz on 11/22/18.
+//  Copyright © 2018 Carlos Santiago Cruz. All rights reserved.
+//
+
+import Foundation
+
+class PhotoInfoController {
+    func fetchPhotoInfo(completion: @escaping (PhotoInfo?) -> Void) {
+        // this function just make a request to nasa api and decode the data into a dictionary
+        // so it is not executed in line with the rest of the code.
+        let baseURL = URL(string: "https://api.nasa.gov/planetary/apod")!
+        let query: [String: String] = ["api_key": "DEMO_KEY"]
+        let url = baseURL.withQueries(query)!
+        let task = URLSession.shared.dataTask(with: url) { (data,response,error) in
+            // this happens after the network request is complete.
+            let jsonDecoder = JSONDecoder()
+            if let data = data, let photoInfo = try? jsonDecoder.decode(PhotoInfo.self, from: data) {
+                completion(photoInfo)
+            } else {
+                print("ERROR: either no data was return, or data was not property decoded")
+                completion(nil)
+            }
+        }
+        task.resume()
+    }
+}
---
+++ WorkingWithTheWeb/RequestNASAapodConcurrency/RequestNASAapod/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  RequestNASAapod
+//
+//  Created by Carlos Santiago Cruz on 11/20/18.
+//  Copyright © 2018 Carlos Santiago Cruz. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class ViewController: UIViewController {
+    @IBOutlet weak var todaysPhoto: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var copyRightLabel: UILabel!
+    
+    let photoInfoController = PhotoInfoController()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        
+        photoInfoController.fetchPhotoInfo { (fetchedInfo) in
+            if let fetchedInfo = fetchedInfo {
+            DispatchQueue.main.async {
+                    self.title = fetchedInfo.title
+                self.descriptionLabel.text = fetchedInfo.description
+            }
+            if let copyRight = fetchedInfo.copyright {
+                self.copyRightLabel.text = "Copyright: \(copyRight)"
+            }   else {
+                self.copyRightLabel.isHidden = true
+            }
+            }
+        }
+        
+    }
+}
+
