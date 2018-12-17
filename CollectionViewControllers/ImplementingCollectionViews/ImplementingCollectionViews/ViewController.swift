83ec2710af8ed710bb85611e077c61becea1bf13
carlos-santiago-2017 <c.santiago.cruz@gmail.com>
2018-12-17 09:35:14 -0600

    Add IBOutlet to the viewController, bookCover

--- CollectionViewControllers/ImplementingCollectionViews/ImplementingCollectionViews/ViewController.swift
+++ CollectionViewControllers/ImplementingCollectionViews/ImplementingCollectionViews/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var bookCover: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
