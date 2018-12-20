36f284ee1b5d996640a27fedca37dd655a3c9466
carlos-santiago-2017 <c.santiago.cruz@gmail.com>
2018-12-19 22:21:49 -0600

    Add example for how to easy is use codable

---
+++ Codable/Encoding-DecodingAndSerializationSwift4/Encoding-DecodingAndSerializationSwift4/Employee.swift
@@ -0,0 +1,18 @@
+//
+//  Employee.swift
+//  Encoding-DecodingAndSerializationSwift4
+//
+//  Created by Carlos Santiago Cruz on 12/19/18.
+//  Copyright © 2018 Carlos Santiago Cruz. All rights reserved.
+//
+
+import Foundation
+
+struct Employee: Codable {
+    var name: String
+    var id: Int
+    var favoriteToy: Toy
+}
+struct Toy: Codable {
+    var name: String
+}
---
+++ Codable/Encoding-DecodingAndSerializationSwift4/Encoding-DecodingAndSerializationSwift4/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Encoding-DecodingAndSerializationSwift4
+//
+//  Created by Carlos Santiago Cruz on 12/19/18.
+//  Copyright © 2018 Carlos Santiago Cruz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let toy1 = Toy(name: "Teddy Bear")
+        let employee1 = Employee(name: "John Appleseed", id: 7, favoriteToy: toy1)
+        //create an instance let jsonEncoder = JSONEncoder()
+        let jsonEncoder = JSONEncoder()
+        let jsonData = try? jsonEncoder.encode(employee1)
+        // Use String initializer
+        let jsonString = String(data: jsonData!, encoding: .utf8)
+        print(jsonString!)
+        let jsonDecoder = JSONDecoder()
+        let employee2 = try? jsonDecoder.decode(Employee.self, from: jsonData!)
+        print(employee2)
+        
+    }
+
+
+}
+
